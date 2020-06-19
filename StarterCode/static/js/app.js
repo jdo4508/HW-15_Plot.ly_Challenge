325c6b79180d65c33ced1ea442fea69f21e0e1eb
unknown <jdo4508@gmail.com>
2020-06-18 21:05:35 -0400

    updated js code

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,13 +1,13 @@
 //Declare function to create bar plot
 function barplot(samples) {
     
-    //Get top 10 values, OTUs and labels
+    //Create variables for top 10 values, OTUs and labels
     var toptenvalues = samples.sample_values.slice(0, 10);
     var toptenOTU = (samples.otu_ids.slice(0, 10));
     var toptenOTU = toptenOTU.map(d => "OTU " + d)
     var labels = samples.otu_labels.slice(0, 10);
     
-    //Create variables for barplot
+    //Create data variable for barplot
     var data = [{
         x: toptenvalues,
         y: toptenOTU,
@@ -16,21 +16,22 @@ function barplot(samples) {
         type:"bar",
         orientation: "h",
     }];
-    // create layout variable for plot
+    //Create layout variable for barplotplot
     var layout = {
         title: "Top 10 OTUs for Selected Subject ID",
         yaxis:{autorange:"reversed",
           tickmode:"linear",
         },
     };
-    // create the bar plot
+    //Create barplot
     Plotly.newPlot("bar", data, layout);
   };
   
   
   //Declare function to create bubbleplot
   function bubbleplot(samples) {
-    // create data variable for plot
+
+    //Create data variable for bubbleplot
     var data = [{
       x: samples.otu_ids,
       y: samples.sample_values,
@@ -41,37 +42,42 @@ function barplot(samples) {
       },
       text: samples.otu_labels
     }];
-    // create layout variable for plot
+
+    //Create layout variable for bubbleplot
     var layout = {
       xaxis:{title: "OTU ID"},
-      height: 600,
+      height: 500,
       width: 1000
     };
-    // create the bubble plot
+    //Create the bubbleplot
     Plotly.newPlot("bubble", data, layout); 
   };
   
   
-  // FUNCTION to create demographic info table
+  //Decalre function to create demographic table
   function table(metadata) {
-    // select demographic info object to put data and empty current selection
+
+    //Create demographic info variable for data 
     var demoinfo = d3.select("#sample-metadata");
     demoinfo.html("");
-    // add demographic information from the new sample
+
+    //Add demographic info for the selection
     Object.entries(metadata).forEach(([key, value]) => {
       demoinfo.append("p").text(`${key}:${value}`);
     });
   }
   
   
-  // FUNCTION to Refresh the dashboard if the sample changes
+  //Declare function to update the dashboard for new selection
   function optionChanged(sample) {
-    // Pull metadata and sample, then filter by dropdown selection
+
+    //Select and create metadata and sample variables for dropdown selection
     d3.json("static/samples.json").then((data)=> {
       var metadata = data.metadata.filter(meta => meta.id.toString() === sample)[0];
       var samples = data.samples.filter(s => s.id.toString() === sample)[0];
       var wfreq=metadata.wfreq
-      // Feed data into display functions
+
+      //Enter data into created plots and summary table
       barplot(samples);
       bubbleplot(samples);
       table(metadata);
@@ -80,20 +86,22 @@ function barplot(samples) {
   };
   
   
-  // FUNCTION to Initialize site by grabbing the names of the dataset and displaying graphs from the first dataset
+  //Declare function to initialize site using data from first metadata set
   function init() {
-    // Grab Dropdown menu and add choices from the dataset
+
+    //Create dropdown menu 
     var dropdown = d3.select("#selDataset"); 
-    // create list of datasets for the dropdown
+
+    //Create datasets for dropdown selection
     d3.json("static/samples.json").then((data)=> {       
       data.names.forEach(function(name) {
       dropdown.append("option").text(name).property("value");
     });
-    // initialize the plots with data from the first dataset
+    //Enter data from first data set
     optionChanged(data.names[0]);
     });
   }
   
   
-  // Run the initialize funciton
+  //Intialize
   init();
